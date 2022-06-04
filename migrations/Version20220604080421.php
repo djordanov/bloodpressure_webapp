b58ccb07e968790f9c6058e256a8b3e9cac67c55
Damian Jordanov <d.jordanov@posteo.net>
2022-06-04 10:05:51 +0200

    create BloodPressureObservation entity

---
+++ migrations/Version20220604080421.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220604080421 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE blood_pressure_observation (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, systolic INTEGER NOT NULL, diastolic INTEGER NOT NULL, pulse INTEGER NOT NULL, comment CLOB DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE blood_pressure_observation');
+    }
+}
---
+++ src/Entity/BloodPressureObservation.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BloodPressureObservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BloodPressureObservationRepository::class)]
+class BloodPressureObservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $systolic;
+
+    #[ORM\Column(type: 'integer')]
+    private $diastolic;
+
+    #[ORM\Column(type: 'integer')]
+    private $pulse;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $comment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSystolic(): ?int
+    {
+        return $this->systolic;
+    }
+
+    public function setSystolic(int $systolic): self
+    {
+        $this->systolic = $systolic;
+
+        return $this;
+    }
+
+    public function getDiastolic(): ?int
+    {
+        return $this->diastolic;
+    }
+
+    public function setDiastolic(int $diastolic): self
+    {
+        $this->diastolic = $diastolic;
+
+        return $this;
+    }
+
+    public function getPulse(): ?int
+    {
+        return $this->pulse;
+    }
+
+    public function setPulse(int $pulse): self
+    {
+        $this->pulse = $pulse;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
